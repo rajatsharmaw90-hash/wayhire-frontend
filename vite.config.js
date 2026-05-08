2aaae1b4d2389b9c509fb34701657485172ee597
Rajat Sharma <rajatsharmaw90gmail.com>
2026-05-08 11:00:31 -0700

    xml added

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemap } from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://www.mywayhire.com",
-    }),
-  ],
+  plugins: [react()],
 })
